7724f84d1e209556c4e9ee60bc5e3ed54e9e57fa
wesllenalves <wesllenalves@gmail.com>
2018-05-24 07:33:28 -0300

    inserindo e incluindo modificações: Reconfigurando o modo como sao chamado o index e o bootstrap da aplicação

--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-   //ini_set("display_errors", "On");
-
-   require_once __DIR__ . DIRECTORY_SEPARATOR . '../vendor/autoload.php';
-   require_once __DIR__ . DIRECTORY_SEPARATOR .'../core/bootstrap.php';
-
-   
-   
