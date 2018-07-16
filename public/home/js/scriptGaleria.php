98c5acb5101b4df7c10a20972be028f12792f1a2
wesllenalves <wesllenalves@gmail.com>
2018-07-16 09:39:44 -0300

    Colocando todas as fotos na galeria. Renomeando as fotos. Criando arquivo separado para script da galeria

---
+++ public/home/js/scriptGaleria.php
@@ -0,0 +1,202 @@
+<script>
+    $(document).ready(function() {
+$('#open-popup').magnificPopup({
+    items: [
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img1.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img2.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img3.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img4.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img5.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img6.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img7.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img8.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img9.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img10.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img11.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img12.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img13.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img14.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img15.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img16.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img17.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img18.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img19.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img20.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img21.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img22.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img23.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img24.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img25.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img26.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img27.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img28.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img29.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img30.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img31.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img32.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img33.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img34.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img35.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img36.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img37.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img38.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img39.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img40.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img41.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img42.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img43.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img44.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img45.jpg',
+        title: ''
+      },
+      {
+        src: '<?= base_url('') ?>/public/img/galeria/img46.jpg',
+        title: ''
+      },
+      
+      
+      
+      
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+
+});
+</script>
+
